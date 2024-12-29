3916802d869342d94a5504278716d255e32ca506
Kibeom Shin <amazing1997@naver.com>
2024-12-29 22:28:18 +0900

    chrome url 제외 코드 추가

--- background.js
+++ background.js
@@ -5,34 +5,40 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.action.onClicked.addListener(async (tab) => {
-  chrome.storage.local.get("badgeState", async (data) => {
-    const prevState = data.badgeState || "OFF";
-    const nextState = prevState === "ON" ? "" : "ON";
+  if (tab.url && !tab.url.startsWith("chrome://")) {
+    chrome.storage.local.get("badgeState", async (data) => {
+      const prevState = data.badgeState || "OFF";
+      const nextState = prevState === "ON" ? "" : "ON";
 
-    chrome.storage.local.set({ badgeState: nextState });
+      chrome.storage.local.set({ badgeState: nextState });
 
-    await chrome.action.setBadgeText({ text: nextState });
-    const badgeColor = nextState === "ON" ? "#819FF7" : "#A4A4A4";
-    chrome.action.setBadgeBackgroundColor({ color: badgeColor });
+      await chrome.action.setBadgeText({ text: nextState });
+      const badgeColor = nextState === "ON" ? "#819FF7" : "#A4A4A4";
+      chrome.action.setBadgeBackgroundColor({ color: badgeColor });
 
-    if (nextState === "ON" && tab.id) {
-      chrome.scripting.executeScript(
-        {
-          target: { tabId: tab.id },
-          files: ["content.js"],
-        },
-        () => {
-          console.log("GTM Clover script executed.");
-        }
-      );
-    } else {
-      console.log("GTM Clover turned OFF. No scripts will be injected.");
-    }
-  });
+      if (nextState === "ON" && tab.id) {
+        chrome.scripting.executeScript(
+          {
+            target: { tabId: tab.id },
+            files: ["content.js"],
+          },
+          () => {
+            console.log("GTM Clover script executed.");
+          }
+        );
+      } else {
+        console.log("GTM Clover turned OFF. No scripts will be injected.");
+      }
+    });
+  }
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete") {
+  if (
+    changeInfo.status === "complete" &&
+    tab.url &&
+    !tab.url.startsWith("chrome://")
+  ) {
     chrome.storage.local.get("badgeState", (data) => {
       if (data.badgeState === "ON" && tab.id) {
         chrome.scripting.executeScript(
